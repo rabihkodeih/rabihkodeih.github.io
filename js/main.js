90f746932f538504bfe2d689fe838a34267ac70a
Rabih Kodeih <rabih.kodeih@gmail.com>
2015-10-13 02:35:05 +0300

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(document).ready( function() {
+
+	// FIXME: Logo
+	// var $logo 	= $('#logo');
+	// if (location.href.indexOf("#") != -1) {
+        // if(location.href.substr(location.href.indexOf("#"))!='#about'){
+        	// $logo.show();
+        // }
+    // }
+    
+	// FIXME: Show logo 
+	// $('#tab-container .tab a').click(function() {
+	  // $logo.slideDown('slow');
+	// });
+	// // Hide logo
+	// $('#tab-about').click(function() {
+	  // $logo.slideUp('slow');
+	// });	
+	
+	function animMeter(){
+	    $(".meter > span").each(function() {
+	                $(this)
+	                	.data("origWidth", $(this).width())
+	                    .width(0).animate({width: $(this).data("origWidth")}, 1200);
+	            });
+	}
+	//FIXME: animMeter();
+
+      $('#tab-container').easytabs({
+        animate			: false,
+        updateHash		: true,
+        transitionIn	: 'slideDown',
+        transitionOut	: 'slideUp',
+        animationSpeed	: 600,
+        tabActiveClass	: 'active'});
+        //FIXME:
+        // .bind('easytabs:midTransition', function(event, $clicked, $targetPanel){
+            // if($targetPanel.selector=='#resume'){
+            	// animMeter();
+            // }
+        // });
+    });
